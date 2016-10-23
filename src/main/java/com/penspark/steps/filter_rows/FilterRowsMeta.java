df53475f3076fc8d8e1f386a5ff700da9b89cc6a
Sriramkumar <engrsriram@gmail.com>
2016-10-23 22:52:36 +0530

    FilterRowsMeta working  with other module

--- src/main/java/com/penspark/steps/filter_rows/FilterRowsMeta.java
+++ src/main/java/com/penspark/steps/filter_rows/FilterRowsMeta.java
@@ -17,13 +17,7 @@ class FilterRowsMeta  {
 	String Filter_Condition = "";
 	//String SqlOpr = "from ComOperation";
 	public FilterRowsMeta(Element element) {
-		 // allocate BaseStepMeta
 		
-		/*
-		 * get True and false options. 
-		 * 
-		 * 
-		 */
 		log.info(element.asXML());
 		log.info(element.elementText("send_true_to"));
 		log.info(element.elementText("send_false_to"));
@@ -89,9 +83,221 @@ class FilterRowsMeta  {
 			}
 			
 			*/
-		this.Filter_Condition = "";
-		log.info(this.Filter_Condition);
+	
+	String result = "";
+	log.info(element.elementText("send_true_to"));
+	log.info(element.elementText("send_false_to"));
+	//log.info(element1.asXML());
+	@SuppressWarnings("unchecked")
+	List<Node> nodes1 = element.selectNodes("compare/condition" );
+	log.info(nodes1.size());
+	Element onlycon =(Element)nodes1.get(0);
+	log.info(onlycon.elementText("negated"));
+	Iterator<Node> iter1=nodes1.iterator();
+	while(iter1.hasNext()){
+		Element element2=(Element)iter1.next();
+		// /conditions/condition 
+		// if the iter1 contains conditions then pass to conditions(element)
+		// else pass same to condition(element)
+		log.info(element2.elementText("negated"));
+		if(element2.elements("conditions").size()> 0){
+			log.info("<<>>");
+			List<Node> nodesqw = element2.selectNodes("conditions" );
+			log.info(nodesqw.size());
+	        Iterator<Node> iter2=nodes1.iterator();
+			while(iter2.hasNext()){
+				Element element3=(Element)iter2.next();
+				result += reCondition(element3);
+			}
+		}
+		else {
+			log.info("condition");
+		}
 	}
+	this.Filter_Condition = result;
+	log.info(this.Filter_Condition);
+	//return result;
+}
+private String reCondition(Element element2) {
+	String result = "";
+
+	log.info("condition:"+ element2.getName());
+	if(element2.getName().equals("condition")){
+		@SuppressWarnings("unchecked")
+		List<Node> nodes1 = element2.selectNodes("conditions" );
+		log.info(":>:"+nodes1.size());
+        Iterator<Node> iter1=nodes1.iterator();
+		while(iter1.hasNext()){
+			
+			log.info("IM HR");
+			
+
+			log.info("conditions+"+ element2.asXML());
+            Element element4=(Element)iter1.next();
+            log.info("C"+element4.elements("conditions").size());
+            if(element4.elements("conditions").size()> 0){
+            	result += reCondition(element4);
+            	log.info(result);           		
+            }
+            else {
+            	result += NodeCondition(element4);
+            	
+            	
+         
+            	
+            	log.info(result); 
+            }
+        }	
+		//result += reCondition(element2);
+	}
+	else{
+		log.info("Its Condition");
+		result += reCondition(element2);
+	}
+//	if(element2.elementText("negated").equals("N"))
+//	{
+//		log.info(element2.getPath());
+//		result = "(" + result + ")";	
+//	}else{
+//		result = "NOT (" + result + ")";	
+//	}
+	if(element2.elementText("negated").equals("Y"))
+	{
+		result = "NOT (" + result + ")";	
+	}
+	log.info("RESULT" + result);
+	return result;
+}
+private String NodeCondition(Element element1) {
+	String result = "";
+	@SuppressWarnings("unchecked")
+	//List<Node> nodes1 = element1.selectNodes("compare/condition/conditions/condition" );
+	List<Node> nodes1 = element1.selectNodes("condition" );
+	log.info(nodes1.size());
+    Iterator<Node> iter1=nodes1.iterator();
+	while(iter1.hasNext()){
+        Element element2=(Element)iter1.next();
+        if(element2.elements("conditions").size()> 0){
+        	//result += reCondition(element2);
+        	
+        	log.info(element2.elements("operator").size());
+        	log.info(element2.elementText("operator"));
+        	if(element2.elements("operator").size()> 0){
+        		String pre = " " + element2.elementText("operator") + " ";
+        		result += pre +"("+ reCondition(element2) + ")";
+        	}
+        	log.info(result);           		
+        } 
+        else
+        	{
+        log.info("Final:" + element2.asXML());
+        log.info("-->:" +element2.elements("rightvalue").size());
+    	if(element2.elements("rightvalue").size()> 0){
+    		log.info(element2.elementText("rightvalue"));
+    		if(!element2.elementText("rightvalue").isEmpty()){
+        	result += rightCol(element2);
+        	
+        	log.info(result);  
+    	 }
+        else {
+        	result += rightColvalue(element2);
+        	log.info(result); 
+        }
+        }
+        else {
+        	result += rightColvalue(element2);
+        	log.info(result); 
+        }
+    	}
+    }
+	//return result;
+	return result;
+}
+private String rightCol(Element element2) {
+	String result = "";
+	String pre = "";
+	
+	switch (element2.elementText("function")) {
+	case "IS NOT NULL":
+	case "IS NULL":
+		result +=  element2.elementText("leftvalue") + " " +element2.elementText("function") + " ";
+		break;
+	case "CONTAINS":
+		result += element2.elementText("leftvalue") + " like CONCAT('%'"+ element2.elementText("rightvalue") + "'%')";
+		break;
+	case "STARTS WITH":
+		result += element2.elementText("leftvalue") + " like CONCAT("+ element2.elementText("rightvalue") + "'%')";
+		break;
+	case "ENDS WITH":
+		result += element2.elementText("leftvalue") + " like CONCAT('%'"+ element2.elementText("rightvalue") + ")";
+		break;
+	default:
+		result += element2.elementText("leftvalue") + " " +element2.elementText("function") + " "+ element2.elementText("rightvalue"); 
+	}
+	
+	if(element2.elements("operator").size()> 0){
+		pre = " " + element2.elementText("operator") + " ";
+		result  = pre +"("+ result + ")";
+	} else{
+		result = " (" + result + ")";		
+	}
+	if(element2.elementText("negated").equals("Y"))
+	{
+		result = "NOT (" + result + ")";	
+	}
+	return result;
+
+}
+private String rightColvalue(Element element2) {
+	String result="";
+	String pre = "";
+	String RightValue = getRightValue(element2);
+	log.info("Value:" + element2.getPath());
+	log.info(element2.asXML());
+	switch (element2.elementText("function")) {
+	case "IS NOT NULL":
+	case "IS NULL":
+		result +=  element2.elementText("leftvalue") + " " +element2.elementText("function") + " ";
+		break;
+	case "CONTAINS":
+		result += element2.elementText("leftvalue") + " like '%"+ RightValue + "%'";
+		break;
+	case "STARTS WITH":
+		result += element2.elementText("leftvalue") + " like '" + RightValue + "%'";
+		break;
+	case "ENDS WITH":
+		result += element2.elementText("leftvalue") + " like '%'"+ RightValue + "'" ;
+		break;
+	default:
+		result += element2.elementText("leftvalue") + " " +element2.elementText("function") + " '" + RightValue + "'"; 
+	}
+	if(element2.elements("operator").size()> 0){
+		pre = " " + element2.elementText("operator") + " ";
+		result  = pre +"("+ result + ")";
+	} else{
+		result = " (" + result + ")";		
+	}
+	if(element2.elementText("negated").equals("Y"))
+	{
+		result = "NOT (" + result + ")";	
+	}
+	return result;
+
+}
+
+private String getRightValue(Element element2) {
+	String result = "";
+	@SuppressWarnings("unchecked")
+	List<Node> nodes1 = element2.selectNodes("value" );
+	log.info(nodes1.size());
+    Iterator<Node> iter1=nodes1.iterator();
+	while(iter1.hasNext()){
+        Element element22=(Element)iter1.next();
+        result = element22.elementText("text");
+	}
+	return result;
+}
+
 	public String getFltrCondition() {
 		return this.Filter_Condition;
 	}
